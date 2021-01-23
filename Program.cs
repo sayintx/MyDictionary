0ddf6724b9b68076248450d97f290a82c1b35b64
sayintx <tamersyn@gmail.com>
2021-01-23 19:44:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> cities = new MyDictionary<string, string>();
+
+            cities.Add("1", "Ankara");
+            cities.Add("2", "İstanbul");
+            cities.Add("3", "İzmir");
+
+            cities.Listing();
+            Console.ReadLine();
+        }
+    }
+}
